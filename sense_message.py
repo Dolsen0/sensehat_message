babb7f117d366cd9b3aa90104def8f7fae73dd89
dolsen0 <derek.olsen@gmail.com>
2022-10-12 15:39:38 -0400

    added scroll speed param

--- sense_message.py
+++ sense_message.py
@@ -3,9 +3,12 @@ sense = SenseHat()
 
 blue = (0,0,255)
 red = (255,0,0)
+yellow = (255, 255, 0)
 green = (0,255,0)
 white = (255,255,255)
 
 sense.clear()
 
-sense.show_message("Hello", text_colour = white, back_colour = blue)
+sense.show_message("Hello", text_colour = white, back_colour = blue, scroll_speed = .1)
+
+sense.clear()
